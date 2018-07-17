153ea5047ad6532f90246f9e866e89fb8ff29f65
Anna Popova <anna_bagulya@mail.ru>
2018-07-18 00:23:55 +0300

    Доработки задания № 3 домашнего задания № 3.1.

--- html-document-structure/tabs/js/tabs.js
+++ html-document-structure/tabs/js/tabs.js
@@ -6,12 +6,40 @@ document.addEventListener('DOMContentLoaded', function() {
   let demoTab = document.querySelector('.tabs-nav li');
   demoTab.remove();
 
+  function tabClick() {
+    for (let article of articles) {
+      article.classList.add('hidden');
+    }
+
+    for (let tab of tabs) {
+      tab.classList.remove('ui-tabs-active');
+    }
+
+    articles[this.dataset['article']].classList.remove('hidden');
+    this.parentElement.classList.add('ui-tabs-active');
+  }
+
+  let counter = 0;
+
   for (let article of articles) {
-    console.log(article.dataset);
     let newTab = demoTab.cloneNode(true);
     const link = newTab.querySelector('a');
     link.innerHTML = article.dataset['tabTitle'];
     link.classList.add(article.dataset['tabIcon']);
+    link.dataset['article'] = counter;
+
     tabsNav.appendChild(newTab);
+    link.addEventListener('click', tabClick);
+
+    counter++;
   }
+
+  let tabs = document.querySelectorAll('.tabs-nav > li');
+
+  for (let article of articles) {
+    article.classList.add('hidden');
+  }
+  
+  articles[0].classList.remove('hidden');
+  tabs[0].classList.add('ui-tabs-active');
 });
