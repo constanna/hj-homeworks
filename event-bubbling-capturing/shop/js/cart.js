8f5b04450cc8c84a983c3ef1840d6f434e0fda22
Anna Popova <anna_bagulya@mail.ru>
2018-07-20 00:12:00 +0300

    Домашнее задание № 3.2. Принцип обработки событий.

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const menu = document.querySelector('.items-list');
+menu.addEventListener('click', function(event) {
+  if (!event.target.classList.contains('add-to-cart')) {
+    return;
+  }
+
+  event.preventDefault();
+  
+  let title = event.target.dataset['title'];
+  let price = event.target.dataset['price'];
+
+  let goods = {
+    title: title,
+    price: price
+  };
+  addToCart(goods);
+});
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (event.target.tagName !== 'TH') {
+    return;
+  }
+
+  let column = event.target.dataset['propName'];
+  const table = document.querySelector('table');
+  table.dataset['sortBy'] = column;
+
+  if (event.target.dataset['sortBy'] == 1) {
+    event.target.dataset['sortBy'] = -1;
+  } else {
+    event.target.dataset['sortBy'] = 1;
+  }
+  sortTable(column, event.target.dataset['sortBy'])
 }
